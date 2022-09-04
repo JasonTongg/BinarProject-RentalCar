c684018c63dd5b2d1587839e405c0f56d9f65178
JasonTongg <aboytong123@gmail.com>
2022-09-05 00:51:42 +0700

    show dashboard statistic

--- src/Sections/Dashboard.js
+++ src/Sections/Dashboard.js
@@ -1,13 +1,14 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import {DashboardContainer, Pwd, Label, InputContainer} from '../Styles/Dashboard'
-import {Bar, getDatasetAtEvent} from 'react-chartjs-2'
+import {Bar} from 'react-chartjs-2'
+import useState from 'react-usestateref'
 
 export default function Dashboard() {
     let [order, setOrder] = useState();
-    let labels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+    let labels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
     let bulan = ["January", "February", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
 
-    let [bulanList, setBulanList] = useState([]);
+    let [bulanList, setBulanList, bulanListRef] = useState([]);
     let [hasil, setHasil] = useState("");
     let [DataStatistik, setDataStatistik] = useState([]);
     let dataStat = [];
@@ -25,78 +26,76 @@ export default function Dashboard() {
     }
 
     let getData = async () => {
-      try {
-        let rawData = await window.fetch("https://bootcamp-rent-car.herokuapp.com/customer/order", {
-          method: "GET",
-          headers: {
-            "access_token": window.localStorage.getItem("Admin Token"),
-            "content-Type": "application/json",
-          },
-        });
-
-        if(rawData.status !== 200){
-          throw new Error(rawData.statusText);
+      if(order){
+      }
+      else{
+        try {
+          let rawData = await window.fetch("https://bootcamp-rent-car.herokuapp.com/customer/order", {
+            method: "GET",
+            headers: {
+              "access_token": window.localStorage.getItem("Admin Token"),
+              "content-Type": "application/json",
+            },
+          });
+  
+          if(rawData.status !== 200){
+            throw new Error(rawData.statusText);
+          }
+  
+          let dataBersih = await rawData.json();
+          setOrder(dataBersih);
+        } catch (error) {
+          console.log(error.message);
         }
-
-        let dataBersih = await rawData.json();
-        setOrder(dataBersih);
-      } catch (error) {
-        console.log(error.message);
       }
     }
 
-    let olahData = () => {
+    let olahData = async () => {
       if(order){
         order.forEach(element => {
           let month = element.start_rent_at.split("T")[0].split("-")[1];
-          if(bulanList.length){
-            let cek = bulanList.filter(item => item === bulan[+month]);
-            if(cek === false){
-              setBulanList([...bulanList, bulan[+month - 1]])
-            }
-          }
-          else{
-            setBulanList([...bulanList, bulan[+month - 1]])
+          let cek = bulanListRef.current.some(item => item === bulan[+month - 1]);
+          if(cek === false){
+            setBulanList([...bulanListRef.current, bulan[+month - 1]]);
           }
         });
+      }
+      if(hasil === undefined || hasil === "" || hasil.length === 0 || hasil === false){
+        setHasil(bulanList[0]);
       }
     }
 
     let olahDataStatistik = () => {
       if(hasil){
         order.forEach(item => {
-          let hari = item.element.start_rent_at.split("T")[0].split("-")[0];
-          if(dataStat[20]){
-            dataStat[20] = 20;
-          }
-          else{
-            dataStat[20] = 20;
+          let hari = item.start_rent_at.split("T")[0].split("-")[2];
+          let month = item.start_rent_at.split("T")[0].split("-")[1];
+          if(hasil === bulan[+month - 1]){
+            if(dataStat[+hari - 1]){
+              dataStat[+hari - 1]+=1;
+            }
+            else{
+              dataStat[+hari - 1] = 1;
+            }
           }
         })
       }
-      console.log(dataStat);
+      setDataStatistik(dataStat);
     }
 
     useEffect(() => {
       getData();
+      olahData();
+      olahDataStatistik();
+    }, [order, hasil])
+
+    useEffect(() => {
+      for(let i=0;i<31;i++){
+        dataStat.push(0);
+      }
     }, [])
 
-    useEffect(() => {
-      olahData();
-    }, [order])
-
-    useEffect(() => {
-      olahDataStatistik();
-    }, [hasil])
-
-    useEffect(() => {
-      if(dataStat){
-        console.log(dataStat);
-        setDataStatistik(dataStat);
-      }
-    }, [dataStat])
-
-    if(bulanList !== []){
+    if(DataStatistik.some(item => item>0)){
       return (
         <DashboardContainer>
           <Pwd>Dashboard {`>`} <span> Dashboard </span></Pwd>
@@ -104,9 +103,9 @@ export default function Dashboard() {
           <p>Month</p>
           <InputContainer>
             <select onChange={(e) => setHasil(e.target.value)}>
-              {bulanList.map(item => {
+              {bulanList.map((item, idx) => {
                 return(
-                  <option value={item}>{item}</option>
+                  <option defaultValue={item} key={idx}>{item}</option>
                 )
               })}
             </select>
