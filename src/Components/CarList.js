a57788d483d41ca12311f2e968697712bd412f7f
JasonTongg <aboytong123@gmail.com>
2022-09-20 21:02:31 +0700

    fix car list loading car bug

--- src/Components/CarList.js
+++ src/Components/CarList.js
@@ -1,119 +1,146 @@
-import React, { useEffect } from 'react'
-import { Container, CarItem, Buttons, Icon, SmallContainer } from '../Styles/CarList'
-import Button from './Button'
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import React, {useEffect} from 'react';
+import {
+  Container,
+  CarItem,
+  Buttons,
+  Icon,
+  SmallContainer,
+} from '../Styles/CarList';
+import Button from './Button';
+import {useNavigate, useSearchParams} from 'react-router-dom';
 import carTemp from '../Assets/carTemp.png';
-import { useSelector } from 'react-redux';
-import '../Styles/cssStyle.css'
-import {BiChevronLeft, BiChevronRight} from 'react-icons/bi'
-import DotLoader from "react-spinners/DotLoader";
-import NotFoundImage from '../Assets/NotFound.jpg'
-import useState from 'react-usestateref'
+import {useSelector} from 'react-redux';
+import '../Styles/cssStyle.css';
+import {BiChevronLeft, BiChevronRight} from 'react-icons/bi';
+import DotLoader from 'react-spinners/DotLoader';
+import NotFoundImage from '../Assets/NotFound.jpg';
+import useState from 'react-usestateref';
 
 function CarList(props) {
-    const [data, setData] = useState([]);
-    const [, setSearch, searchRef] = useState([]);
-    let navigate = useNavigate();
-    let [value] = useSearchParams();
-    let carData = useSelector(state => state.items.CarList);
-    let [loading, setLoading] = useState(true);
-    let [, setCutData, cutDataRef] = useState();
-    let [, setPosisi, posisiRef] = useState(0);
-    let [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [, setSearch, searchRef] = useState([]);
+  let navigate = useNavigate();
+  let [value] = useSearchParams();
+  let carData = useSelector((state) => state.items.CarList);
+  let [loading, setLoading] = useState(true);
+  let [, setCutData, cutDataRef] = useState();
+  let [, setPosisi, posisiRef] = useState(0);
+  let [isLoading, setIsLoading] = useState(true);
 
-    let directDetails = (id) => {
-        navigate(`/details/${id}`);
+  let directDetails = (id) => {
+    navigate(`/details/${id}`);
+  };
+
+  useEffect(() => {
+    if (value) {
+      let name = value.get('name');
+      let category = value.get('category');
+      let price = value.get('price');
+      setSearch([name, category, price]);
+    }
+  }, [value, searchRef, setSearch]);
+
+  useEffect(() => {
+    setLoading(true);
+    if (searchRef.current[0] !== undefined && props.empty === undefined) {
+      let filterData = carData.filter(
+        (item) =>
+          item.category === searchRef.current[1] &&
+          item.name.toLowerCase() === searchRef.current[0].toLowerCase()
+      );
+
+      if (searchRef.current[2] === '< Rp. 400.000') {
+        filterData = filterData.filter((item) => item.price < 400000);
+      } else if (searchRef.current[2] === 'Rp. 400.000 - Rp. 600.000') {
+        filterData = filterData.filter(
+          (item) => item.price >= 400000 && item.price <= 600000
+        );
+      } else {
+        filterData = filterData.filter((item) => item.price > 600000);
+      }
+
+      setData(filterData);
+    } else {
+      setData(carData);
     }
 
-    useEffect(() => {
-        if(value){
-            let name = value.get("name");
-            let category = value.get("category");
-            let price = value.get("price");
-            setSearch([name, category, price]);
-        }
-    }, [value, searchRef, setSearch])
-
-    useEffect(() => {
-        setLoading(true);
-        if(searchRef.current[0] !== undefined && props.empty === undefined){
-            let filterData = carData.filter(item => item.category === searchRef.current[1] && item.name.toLowerCase() === searchRef.current[0].toLowerCase());
-
-            if (searchRef.current[2] === "< Rp. 400.000") {
-                filterData = filterData.filter(item => item.price < 400000);
-            }
-            else if (searchRef.current[2] === "Rp. 400.000 - Rp. 600.000") {
-                filterData = filterData.filter(item => item.price >= 400000 && item.price <= 600000);
-            }
-            else {
-                filterData = filterData.filter(item => item.price > 600000);
-            }   
-
-            setData(filterData);
-        }
-        else{
-            setData(carData);
-        }
-
-        let cut = [];
-        for(let i=0;i<data.length;i+=9){
-            cut.push(data.slice(i,i+9));
-        }
-        setCutData(cut);
-
-        if(cutDataRef.current){
-            setLoading(false);
-        }
-    },[carData, cutDataRef, props.empty, setCutData, data, setData, searchRef])
-
-    useEffect(() => {
-        setIsLoading(true);
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 5000)
-    }, [loading])
-
-    if(loading === false) {
-        return (
-            <>
-                <Container className='carList' show>
-                    {cutDataRef.current[posisiRef.current]?.map(item => 
-                    (
-                        <CarItem key={item.id}>
-                            <img src={item.image ? item.image : carTemp} alt="" />
-                            <h5>{item.name}</h5>
-                            <h3>Rp {item.price},-</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                            <Button size="big" action={() => directDetails(item.id)} idCar={item.id}>Pilih Mobil</Button>
-                        </CarItem>
-                    )
-                    )}
-                </Container>
-                {cutDataRef.current[posisiRef.current] ? <Buttons>
-                    {posisiRef.current === 0 ? null : (<Icon onClick={() => setPosisi(posisiRef.current-1)}><BiChevronLeft></BiChevronLeft></Icon>)}
-                    {posisiRef.current === cutDataRef.current.length-1 ? null : (<Icon onClick={() => setPosisi(posisiRef.current+1)}><BiChevronRight></BiChevronRight></Icon>)}
-                </Buttons> : null}
-            </>
-        )
+    let cut = [];
+    for (let i = 0; i < data.length; i += 9) {
+      cut.push(data.slice(i, i + 9));
     }
-    else {
-        if(isLoading === true){
-            return (
-                <Container className='loadContainer carList' show>
-                    <DotLoader color={"#D0d0d0"} loading={loading} size={100}/>
-                </Container>
-            )
-        }
-        else{
-            return (
-                <SmallContainer className='loadContainer carList'>
-                    <img src={NotFoundImage} alt="Not Found" />
-                    <h1 className='errorh1'>Waduh mobil yang anda cari nga ada!!</h1>
-                    <p className='errorp'>Coba cari tipe atau kategori lain...</p>
-                </SmallContainer>
-            )
-        }
+    setCutData(cut);
+
+    if (cutDataRef.current) {
+      setLoading(false);
     }
+  }, [carData, cutDataRef, props.empty, setCutData, data, setData, searchRef]);
+
+  useEffect(() => {
+    console.log('load');
+    setIsLoading(true);
+    setTimeout(() => {
+      console.log('sudah');
+      setIsLoading(false);
+    }, 5000);
+  }, [loading]);
+
+  if (loading === false && cutDataRef.current.length !== 0) {
+    console.log(cutDataRef.current.length);
+    return (
+      <>
+        <Container className="carList" show>
+          {cutDataRef.current[posisiRef.current]?.map((item) => (
+            <CarItem key={item.id}>
+              <img src={item.image ? item.image : carTemp} alt="" />
+              <h5>{item.name}</h5>
+              <h3>Rp {item.price},-</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.{' '}
+              </p>
+              <Button
+                size="big"
+                action={() => directDetails(item.id)}
+                idCar={item.id}
+              >
+                Pilih Mobil
+              </Button>
+            </CarItem>
+          ))}
+        </Container>
+        {cutDataRef.current[posisiRef.current] ? (
+          <Buttons>
+            {posisiRef.current === 0 ? null : (
+              <Icon onClick={() => setPosisi(posisiRef.current - 1)}>
+                <BiChevronLeft></BiChevronLeft>
+              </Icon>
+            )}
+            {posisiRef.current === cutDataRef.current.length - 1 ? null : (
+              <Icon onClick={() => setPosisi(posisiRef.current + 1)}>
+                <BiChevronRight></BiChevronRight>
+              </Icon>
+            )}
+          </Buttons>
+        ) : null}
+      </>
+    );
+  } else {
+    if (isLoading === true) {
+      return (
+        <Container className="loadContainer carList" show>
+          <DotLoader color={'#D0d0d0'} loading={isLoading} size={100} />
+        </Container>
+      );
+    } else {
+      return (
+        <SmallContainer className="loadContainer carList">
+          <img src={NotFoundImage} alt="Not Found" />
+          <h1 className="errorh1">Waduh mobil yang anda cari nga ada!!</h1>
+          <p className="errorp">Coba cari tipe atau kategori lain...</p>
+        </SmallContainer>
+      );
+    }
+  }
 }
 
 export default CarList;
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,153 +1,219 @@
-import React from 'react'
-import {Container, Label, FormItem, Option, Input, Sewa, Select, Selects, Inputs, BigContainer, Title, Labels, Sewas} from '../Styles/SearchBar'
-import Button from './Button'
-import {FiCheckCircle} from 'react-icons/fi'
-import {BsChevronDown} from 'react-icons/bs'
+import React from 'react';
+import {
+  Container,
+  Label,
+  FormItem,
+  Option,
+  Input,
+  Sewa,
+  Select,
+  Selects,
+  Inputs,
+  BigContainer,
+  Title,
+  Labels,
+  Sewas,
+} from '../Styles/SearchBar';
+import Button from './Button';
+import {FiCheckCircle} from 'react-icons/fi';
+import {BsChevronDown} from 'react-icons/bs';
 import {useNavigate, createSearchParams} from 'react-router-dom';
-import useState from 'react-usestateref'
+import useState from 'react-usestateref';
 
 export default function SearchBar(props) {
-    let [, setData, dataRef] = useState({});
-    const navigate = useNavigate();
+  let [, setData, dataRef] = useState({});
+  const navigate = useNavigate();
 
-    const toResult = (e) => {
-        e.preventDefault();
-        navigate({
-            pathname: "/result",
-            search: createSearchParams({
-                name: dataRef.current.name,
-                category: dataRef.current.category,
-                price: dataRef.current.price,
-            }).toString()
-        });
-    }
+  const toResult = (e) => {
+    e.preventDefault();
+    navigate({
+      pathname: '/result',
+      search: createSearchParams({
+        name: dataRef.current.name,
+        category: dataRef.current.category,
+        price: dataRef.current.price,
+      }).toString(),
+    });
+  };
 
-    if(props.search){
-        return (
-            <BigContainer className="searchBar">
-                <Container onSubmit={(e) => toResult(e, true)}>
-                    <FormItem>
-                        <Label htmlFor="nama">Nama Mobil</Label>
-                        <Input id="nama" type="text" placeholder='Ketik nama/tipe mobil' required onChange={(e) => setData({
-                            ...dataRef.current,
-                            name: e.target.value,
-                        })}/>
-                    </FormItem>
-                    <FormItem>
-                        <Label htmlFor="kategori">Kategori</Label>
-                        <Select required id="kategori" onChange={(e) => setData({
-                            ...dataRef.current,
-                            category: e.target.value,
-                        })}>
-                            <Option defaultValue="2 - 4 orang">Masukan Kapasitas Mobil</Option>
-                            <Option defaultValue="2 - 4 orang">2 - 4 orang</Option>
-                            <Option defaultValue="4 - 6 orang">4 - 6 orang</Option>
-                            <Option defaultValue="6 - 8 orang">6 - 8 orang</Option>
-                        </Select>
-                    </FormItem>
-                    <FormItem>
-                        <Label htmlFor="harga">Harga</Label>
-                        <Select id="harga" required onChange={(e) => setData({
-                            ...dataRef.current,
-                            price: e.target.value,
-                        })}>
-                            <Option disabled defaultValue="< Rp. 400.000">Masukan Harga Sewa Per Hari</Option>
-                            <Option defaultValue="< Rp. 400.000">{'< Rp. 400.000'}</Option>
-                            <Option defaultValue="Rp. 400.000 - Rp. 600.000">{'Rp. 400.000 - Rp. 600.000'}</Option>
-                            <Option defaultValue="> Rp. 600.000">{'> Rp. 600.000'}</Option>
-                        </Select>
-                    </FormItem>
-                    <FormItem>
-                        <Label htmlFor="status">Status</Label>
-                        <Select id="status" required>
-                            <Option defaultValue="">Disewa</Option>
-                        </Select>
-                    </FormItem>
-                    <Button submit>Cari Mobil</Button>
-                </Container>
-            </BigContainer>
-        )
-    }
-    else if(props.edits){
-        return (
-            <BigContainer className="searchBar">
-                <Title>Pencarianmu</Title>
-                <Container onSubmit={toResult}>
-                    <FormItem>
-                        <Label htmlFor="nama">Nama Mobil</Label>
-                        <Inputs id="nama" type="text" placeholder='Ketik nama/tipe mobil' required onChange={(e) => setData({
-                            ...dataRef.current,
-                            name: e.target.value,
-                        })}/>
-                    </FormItem>
-                    <FormItem>
-                        <Label htmlFor="kategori">Kategori</Label>
-                        <Selects id="kategori" required onChange={(e) => setData({
-                            ...dataRef.current,
-                            category: e.target.value,
-                        })}>
-                            <Option defaultValue="Masukan Kapasitas Mobil">Masukan Kapasitas Mobil</Option>
-                            <Option defaultValue="2 - 4 orang">2 - 4 orang</Option>
-                            <Option defaultValue="4 - 6 orang">4 - 6 orang</Option>
-                            <Option defaultValue="6 - 8 orang">6 - 8 orang</Option>
-                        </Selects>
-                    </FormItem>
-                    <FormItem>
-                        <Label htmlFor="harga">Harga</Label>
-                        <Selects id="harga" required onChange={(e) => setData({
-                            ...dataRef.current,
-                            price: e.target.value,
-                        })}>
-                            <Option defaultValue="Masukan Harga Sewa Per Hari">Masukan Harga Sewa Per Hari</Option>
-                            <Option defaultValue="< Rp. 400.000">{'< Rp. 400.000'}</Option>
-                            <Option defaultValue="Rp. 400.000 - Rp. 600.000">{'Rp. 400.000 - Rp. 600.000'}</Option>
-                            <Option defaultValue="> Rp. 600.000">{'> Rp. 600.000'}</Option>
-                        </Selects>
-                    </FormItem>
-                    <FormItem>
-                        <Label htmlFor="status">Status</Label>
-                        <Sewa>
-                            <p>Disewa</p>
-                            <FiCheckCircle className='icon'></FiCheckCircle>
-                        </Sewa>
-                    </FormItem>
-                    <Button second submit>Edit</Button>
-                </Container>
-            </BigContainer>
-        )
-    }
-    else{
-        return (
-            <BigContainer className="searchBar">
-                <Title>Pencarianmu</Title>
-                <Container>
-                    <FormItem>
-                        <Labels htmlFor="status">Nama Mobil</Labels>
-                        <Sewas></Sewas>
-                    </FormItem>
-                    <FormItem>
-                        <Labels htmlFor="status">Kategori</Labels>
-                        <Sewas>
-                            <p></p>
-                            <BsChevronDown className='icon'></BsChevronDown>
-                        </Sewas>
-                    </FormItem>
-                    <FormItem>
-                        <Labels htmlFor="status">Harga Sewa per Hari</Labels>
-                        <Sewas>
-                            <p></p>
-                            <BsChevronDown className='icon'></BsChevronDown>
-                        </Sewas>
-                    </FormItem>
-                    <FormItem>
-                        <Labels htmlFor="status">Status</Labels>
-                        <Sewas>
-                            <p></p>
-                            <BsChevronDown className='icon'></BsChevronDown>
-                        </Sewas>
-                    </FormItem>
-                </Container>
-            </BigContainer>
-        )
-    }
+  if (props.search) {
+    return (
+      <BigContainer className="searchBar">
+        <Container onSubmit={(e) => toResult(e, true)}>
+          <FormItem>
+            <Label htmlFor="nama">Nama Mobil</Label>
+            <Input
+              id="nama"
+              type="text"
+              placeholder="Ketik nama/tipe mobil"
+              required
+              onChange={(e) =>
+                setData({
+                  ...dataRef.current,
+                  name: e.target.value,
+                })
+              }
+            />
+          </FormItem>
+          <FormItem>
+            <Label htmlFor="kategori">Kategori</Label>
+            <Select
+              required
+              id="kategori"
+              onChange={(e) =>
+                setData({
+                  ...dataRef.current,
+                  category: e.target.value,
+                })
+              }
+            >
+              <Option defaultValue="2 - 4 orang">
+                Masukan Kapasitas Mobil
+              </Option>
+              <Option defaultValue="2 - 4 orang">2 - 4 orang</Option>
+              <Option defaultValue="4 - 6 orang">4 - 6 orang</Option>
+              <Option defaultValue="6 - 8 orang">6 - 8 orang</Option>
+            </Select>
+          </FormItem>
+          <FormItem>
+            <Label htmlFor="harga">Harga</Label>
+            <Select
+              id="harga"
+              required
+              onChange={(e) =>
+                setData({
+                  ...dataRef.current,
+                  price: e.target.value,
+                })
+              }
+            >
+              <Option disabled defaultValue="< Rp. 400.000">
+                Masukan Harga Sewa Per Hari
+              </Option>
+              <Option defaultValue="< Rp. 400.000">{'< Rp. 400.000'}</Option>
+              <Option defaultValue="Rp. 400.000 - Rp. 600.000">
+                {'Rp. 400.000 - Rp. 600.000'}
+              </Option>
+              <Option defaultValue="> Rp. 600.000">{'> Rp. 600.000'}</Option>
+            </Select>
+          </FormItem>
+          <FormItem>
+            <Label htmlFor="status">Status</Label>
+            <Select id="status" required>
+              <Option defaultValue="">Disewa</Option>
+            </Select>
+          </FormItem>
+          <Button submit>Cari Mobil</Button>
+        </Container>
+      </BigContainer>
+    );
+  } else if (props.edits) {
+    return (
+      <BigContainer className="searchBar">
+        <Title>Pencarianmu</Title>
+        <Container onSubmit={toResult}>
+          <FormItem>
+            <Label htmlFor="nama">Nama Mobil</Label>
+            <Inputs
+              id="nama"
+              type="text"
+              placeholder="Ketik nama/tipe mobil"
+              required
+              onChange={(e) =>
+                setData({
+                  ...dataRef.current,
+                  name: e.target.value,
+                })
+              }
+            />
+          </FormItem>
+          <FormItem>
+            <Label htmlFor="kategori">Kategori</Label>
+            <Selects
+              id="kategori"
+              required
+              onChange={(e) =>
+                setData({
+                  ...dataRef.current,
+                  category: e.target.value,
+                })
+              }
+            >
+              <Option defaultValue="Masukan Kapasitas Mobil">
+                Masukan Kapasitas Mobil
+              </Option>
+              <Option defaultValue="2 - 4 orang">2 - 4 orang</Option>
+              <Option defaultValue="4 - 6 orang">4 - 6 orang</Option>
+              <Option defaultValue="6 - 8 orang">6 - 8 orang</Option>
+            </Selects>
+          </FormItem>
+          <FormItem>
+            <Label htmlFor="harga">Harga</Label>
+            <Selects
+              id="harga"
+              required
+              onChange={(e) =>
+                setData({
+                  ...dataRef.current,
+                  price: e.target.value,
+                })
+              }
+            >
+              <Option defaultValue="Masukan Harga Sewa Per Hari">
+                Masukan Harga Sewa Per Hari
+              </Option>
+              <Option defaultValue="< Rp. 400.000">{'< Rp. 400.000'}</Option>
+              <Option defaultValue="Rp. 400.000 - Rp. 600.000">
+                {'Rp. 400.000 - Rp. 600.000'}
+              </Option>
+              <Option defaultValue="> Rp. 600.000">{'> Rp. 600.000'}</Option>
+            </Selects>
+          </FormItem>
+          <FormItem>
+            <Label htmlFor="status">Status</Label>
+            <Sewa>
+              <p>Disewa</p>
+              <FiCheckCircle className="icon"></FiCheckCircle>
+            </Sewa>
+          </FormItem>
+          <Button second submit>
+            Edit
+          </Button>
+        </Container>
+      </BigContainer>
+    );
+  } else {
+    return (
+      <BigContainer className="searchBar">
+        <Title>Pencarianmu</Title>
+        <Container>
+          <FormItem>
+            <Labels htmlFor="status">Nama Mobil</Labels>
+            <Sewas></Sewas>
+          </FormItem>
+          <FormItem>
+            <Labels htmlFor="status">Kategori</Labels>
+            <Sewas>
+              <p></p>
+              <BsChevronDown className="icon"></BsChevronDown>
+            </Sewas>
+          </FormItem>
+          <FormItem>
+            <Labels htmlFor="status">Harga Sewa per Hari</Labels>
+            <Sewas>
+              <p></p>
+              <BsChevronDown className="icon"></BsChevronDown>
+            </Sewas>
+          </FormItem>
+          <FormItem>
+            <Labels htmlFor="status">Status</Labels>
+            <Sewas>
+              <p></p>
+              <BsChevronDown className="icon"></BsChevronDown>
+            </Sewas>
+          </FormItem>
+        </Container>
+      </BigContainer>
+    );
+  }
 }
